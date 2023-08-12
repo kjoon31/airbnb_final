253260d73fd3d3046c368c7f9ba14adf4b8da921
daniel joonkyu kim <danielkim@daniels-mbp.lan>
2023-08-12 16:35:06 -0700

    Initial auth

---
+++ backend/routes/api/index.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+router.use(restoreUser);
+
+router.get('/hello/world', function(req, res) {
+  // res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
