6da3d585fd9e1d6868012021863f6618b3aa3e43
daniel joonkyu kim <danielkim@daniels-mbp.lan>
2023-08-13 20:03:44 -0700

    Migrations references

--- backend/db/migrations/20230813195836-create-spot.js
+++ backend/db/migrations/20230813195836-create-spot.js
@@ -14,7 +14,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       ownerId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id"
+        }
       },
       address: {
         type: Sequelize.STRING
--- backend/db/migrations/20230813203939-create-spot-image.js
+++ backend/db/migrations/20230813203939-create-spot-image.js
@@ -15,7 +15,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       spotId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Spots",
+          key: "id"
+        }
       },
       url: {
         type: Sequelize.STRING
